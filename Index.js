8aeb04e7cfb1b47e53adddc6fc90ebb56291c027
Dane Kargis <dane.kargis@gmail.com>
2023-04-22 19:13:13 -0600

    Added dependincies to the git ignore and added the questions to the js

--- Index.js
+++ Index.js
@@ -1,13 +1,59 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {   
+    message: "What is the name of the project?",
+    name: "title"
+},
+{   
+    message: "Please provide a table on content",
+    name: "table of content"
+},
+{   
+    message: "What is the name of the user?",
+    name: "userName"
+},
+{  
+    message: "Please provide a description of the project",
+    name: "description"
+},
+{   
+    message: "What is the installation process?",
+    name: "installation"
+},
+{   
+    message: "How will this project be used?",
+    name: "usage"
+},
+{   
+    message: "What licenses are required with this project?",
+    name: "licenses"
+},
+{   
+    message: "Who were the contributors to this project?",
+    name: "contribution"
+},
+{   
+    message: "What is the test process for this project?",
+    name: "test"
+},
+{   
+    message: "What is the user github email address?",
+    name: "GitHub user email"
+},
+{   
+    message: "Please provide a profile picture",
+    name: "GitHub profile picture"
+}
 
+];
 // TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
-
 // Function call to initialize app
+
 init();
